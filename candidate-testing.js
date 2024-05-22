9f9c2bf9335e958942048652fc8e0d4238fc9f61
Roxana Pichardo <roxypichardo@gmail.com>
2024-05-21 21:52:11 -0500

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]; 
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]; 
+let candidateAnswers = []; 
 
 
 function askForName() {
@@ -24,19 +24,28 @@ candidateName = input.question("Enter your name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)("Please provide answer");
+//candidateAnswer = input.question(question)("Please provide answer");
+for (let i = 0; i < questions.length;  i++) {
+let response = (input.question(questions[i]))
+candidateAnswers.push(response)}
+
+
 
 }
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("Good Job, that is correct!")
-} else {
-  console.log("Incorrect!")
+//if (candidateAnswer == correctAnswer) {
+ // console.log("Good Job, that is correct!")
+//} else {
+ // console.log("Incorrect!")
+//}
+for (let i = 0; i < questions.length; i++){
+  console.log('Your Answer: ${candidateAnswers[i]]'); 
+  console.log('Correct Answer: ${correctAnswers[i]}'); 
 }
-
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -52,6 +61,7 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
+// practice job
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
@@ -63,4 +73,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}; 
